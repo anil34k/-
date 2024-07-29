ece7c40268c573ee15cc1f3254c397753a359504
ab1-cmd <cab031174@gmail.com>
2024-07-29 21:07:01 +0530

    Add files via upload

---
+++ Cal.js
@@ -0,0 +1,28 @@
+    function clr()
+{
+    num1.value="";
+    num2.value="";
+    result.value="";
+}
+function Showresult(choice)
+{
+    var n1=parseFloat(document.getElementById('num1').value);
+    var n2=parseFloat(document.getElementById('num2').value);
+    var r;
+    var c=choice;
+    switch(c)
+    {
+        case '1':r=n1+n2;
+                    break;
+        case '2':r=n1-n2;
+                    break;
+        case '3':r=n1*n2;
+                    break;
+        case '4':r=n1/n2;
+                    break;
+        default:break;
+    }
+    document.getElementById('result').value=r;
+}
+
+
